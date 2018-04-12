b683828263f1b584e9b8ac92fc86226594db9f83
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-04-11 20:09:09 -0500

    .

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,44 +1,49 @@
+
 class ProductsController < ApplicationController
 
   def index
     @product = Product.all
   end
 
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+    flash[:success] = "Great! Product created!"
+    redirect_to product_path(@product)
+    else
+    redirect_to :back
+    end
+  end
+
   def new
     @product = Product.new
   end
-
-  def create
-    @product = Product.create(product_params)
-    redirect_to product_path(@product)
-  end
-
-  def show
-    @product = Product.find(params[:id])
-  end
-
 
   def edit
     @product = Product.find(params[:id])
   end
 
 
-  def new
-    @product = Product.new(product_params)
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.update(product_params)
   end
 
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to product_path(@product)
-  end
-
+    redirect_to products_path
+    end
 
 
 private
   def product_params
-    params.permit(:name,:description,:price)
+    params.require(:product).permit(:name, :price, :description)
   end
+
 
 end
